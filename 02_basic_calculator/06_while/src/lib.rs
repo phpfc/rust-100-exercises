baae5bcab4f86c63a9f67503ed32eff1a589af24
Pedro Henrique P. França C. (php fc) <pedrofranca-tech@proton.me>
2025-12-15 19:42:11 -0300

    All the way into 03/03

---
+++ 02_basic_calculator/06_while/src/lib.rs
@@ -0,0 +1,43 @@
+// Rewrite the factorial function using a `while` loop.
+pub fn factorial(n: u32) -> u32 {
+    // The `todo!()` macro is a placeholder that the compiler
+    // interprets as "I'll get back to this later", thus
+    // suppressing type errors.
+    // It panics at runtime.
+    let mut i = n;
+    let mut factorial = n;
+
+    if n == 0 || n == 1 {
+        return 1;
+    }
+    while i > 1 {
+        factorial *= i - 1;
+        i -= 1
+    }
+    factorial
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::factorial;
+
+    #[test]
+    fn first() {
+        assert_eq!(factorial(0), 1);
+    }
+
+    #[test]
+    fn second() {
+        assert_eq!(factorial(1), 1);
+    }
+
+    #[test]
+    fn third() {
+        assert_eq!(factorial(2), 2);
+    }
+
+    #[test]
+    fn fifth() {
+        assert_eq!(factorial(5), 120);
+    }
+}
